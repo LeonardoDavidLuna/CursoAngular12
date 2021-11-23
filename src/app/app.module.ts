873b936bbfe6af6c06b74fcd04b69a2fab73f416
Nicolás Luna <leonard_dave_nick@hotmail.com>
2021-11-23 08:53:45 -0600

    Navbar y Componentes creados aún sin lógica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { Home } from './components/home/home.component/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Home,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
